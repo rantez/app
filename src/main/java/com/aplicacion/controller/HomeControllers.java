3ed72c22c3dad696da6e555912a09c53937698be
RDO <rantez@gmail.com>
2019-10-16 02:10:06 -0300

    inicial

---
+++ src/main/java/com/aplicacion/controller/HomeControllers.java
@@ -0,0 +1,18 @@
+package com.aplicacion.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeControllers {
+
+
+
+    @GetMapping("/version")
+    public String getVersion(){
+        return "Vesion 1.0";
+    }
+
+
+}
